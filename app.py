29d06c2aab23bea68955f86f2bc9d085427ab979
Imesh Sandeepa <vexoraitsolution@gmail.com>
2026-05-01 21:03:53 +0530

    update

--- app.py
+++ app.py
@@ -26,8 +26,8 @@ except ImportError:
 # ─────────────────────────────────────────
 DB_HOST     = os.environ.get("DB_HOST",     "localhost")
 DB_NAME     = os.environ.get("DB_NAME",     "qr_gen1")
-DB_USER     = os.environ.get("DB_USER",     "vexorait")
-DB_PASS     = os.environ.get("DB_PASS",     "qrgen_vexora@2007")          # never hardcode!
+DB_USER     = os.environ.get("DB_USER",     "postgres")
+DB_PASS     = os.environ.get("DB_PASS",     "20071224")          # never hardcode!
 ADMIN_KEY   = os.environ.get("ADMIN_KEY",   "1224")          # must be set in production
 SECRET      = os.environ.get("LICENSE_SECRET", "49add1432f7c9f4726a960fe94bdf349203e35806e8a44aa47471d9525cf8673")       # must be set in production
 GRACE_DAYS  = int(os.environ.get("GRACE_DAYS", "3"))
@@ -53,8 +53,8 @@ def index():
     return render_template('index.html')
 
 @app.route('/qr-admin')
-def admin():
-    return render_template('qr_admin.html')    
+def qr_admin():
+    return render_template('qr_admin.html')
 
 # ─────────────────────────────────────────
 # CONNECTION POOL  (replaces single conn)
